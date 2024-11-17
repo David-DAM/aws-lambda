35fecfb62ed393a58cc3968840d695bd37d2afad
David <david11jv@gmail.com>
2024-11-17 11:45:31 +0100

    Add terraform lambda, policies, roles and api gateway integration

--- terraform/main.tf
+++ terraform/main.tf
@@ -103,11 +103,43 @@ resource "aws_s3_object" "products-lambda-code" {
   etag = filemd5("../target/aws-lambda-1.0-SNAPSHOT-lambda-package.zip")
 }
 
+resource "aws_iam_policy" "products-lambda-permissions" {
+  name        = "TerraformLambdaPermissions"
+  description = "Policy to create lambda functions"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect : "Allow"
+        Action : [
+          "lambda:CreateFunction",
+          "lambda:TagResource",
+          "lambda:PublishVersion"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect : "Allow"
+        Action : "iam:PassRole"
+        Resource = aws_iam_role.products-lambda.arn
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_policy_attachment" "products-lambda-permissions" {
+  name       = "policy-lamba-permissions"
+  policy_arn = aws_iam_policy.products-lambda-permissions.arn
+  roles = [aws_iam_role.products-lambda.name]
+}
+
 resource "aws_iam_role" "products-lambda" {
-  name = "serverless_lambda"
+  name = "products-lambda"
 
   assume_role_policy = jsonencode({
-    Version = "2024-10-13"
+    Version = "2012-10-17"
     Statement = [
       {
         Action = "sts:AssumeRole"
@@ -119,6 +151,7 @@ resource "aws_iam_role" "products-lambda" {
       }
     ]
   })
+
 }
 
 resource "aws_iam_role_policy_attachment" "products-lambda" {
@@ -138,4 +171,118 @@ resource "aws_lambda_function" "products" {
   source_code_hash = aws_s3_object.products-lambda-code.key
 
   role = aws_iam_role.products-lambda.arn
+}
+
+resource "aws_iam_policy" "products-lambda-gw" {
+  name        = "APIGatewayPermissionsPolicy"
+  description = "Permisos necesarios para gestionar API Gateway"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect : "Allow",
+        Action : [
+          "apigateway:POST",
+          "apigateway:GET",
+          "apigateway:PUT",
+          "apigateway:DELETE",
+          "apigateway:PATCH",
+          "apigateway:TagResource"
+        ],
+        Resource : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "products-lambda-gw" {
+  name       = "AttachAPIGatewayPolicy"
+  policy_arn = aws_iam_policy.products-lambda-gw.arn
+  roles = [aws_iam_role.products-lambda-gw.name]
+}
+
+resource "aws_iam_role" "products-lambda-gw" {
+  name = "APIGatewayExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "apigateway.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Crea el API Gateway REST
+resource "aws_apigateway_rest_api" "lambda-products" {
+  name        = "lambda-products-agw"
+  description = "API Gateway REST vinculada a Lambda"
+}
+
+# Crea el recurso raíz (ANY)
+resource "aws_apigateway_method" "lambda-products" {
+  rest_api_id   = aws_apigateway_rest_api.lambda-products.id
+  resource_id   = aws_apigateway_rest_api.lambda-products.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+# Integra Lambda con el recurso raíz (ANY)
+resource "aws_apigateway_integration" "lambda-products" {
+  rest_api_id             = aws_apigateway_rest_api.lambda-products.id
+  resource_id             = aws_apigateway_rest_api.lambda-products.root_resource_id
+  http_method             = aws_apigateway_method.lambda-products.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.example_lambda.arn}/invocations"
+}
+
+# Crea un recurso de ejemplo (/{proxy+})
+resource "aws_apigateway_resource" "lambda-products" {
+  rest_api_id = aws_apigateway_rest_api.lambda-products.id
+  parent_id   = aws_apigateway_rest_api.lambda-products.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+# Crea el método ANY para el recurso {proxy+}
+resource "aws_apigateway_method" "lambda-products" {
+  rest_api_id   = aws_apigateway_rest_api.lambda-products.id
+  resource_id   = aws_apigateway_resource.lambda-products.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+# Integra Lambda con el recurso {proxy+}
+resource "aws_apigateway_integration" "lambda-products-resource" {
+  rest_api_id             = aws_apigateway_rest_api.lambda-products.id
+  resource_id             = aws_apigateway_resource.lambda-products.id
+  http_method             = aws_apigateway_method.lambda-products.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.example_lambda.arn}/invocations"
+}
+
+# Despliega el API Gateway
+resource "aws_apigateway_deployment" "lambda-products" {
+  depends_on = [
+    aws_apigateway_integration.lambda-products-resource,
+    aws_apigateway_integration.lambda-products-resource
+  ]
+  rest_api_id = aws_apigateway_rest_api.lambda-products.id
+  stage_name  = "dev"
+}
+
+# Da permisos a API Gateway para invocar la Lambda
+resource "aws_lambda_permission" "apigateway_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.example_lambda.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigateway_rest_api.lambda-products.execution_arn}/*"
 }
